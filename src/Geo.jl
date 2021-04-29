51436c05d6b3ed71ec1982740b7d47eab818936c
Lucca Fraser <lucca.fraser@gmail.com>
2021-04-29 15:30:14 -0300

    tracer

--- src/Geo.jl
+++ src/Geo.jl
@@ -148,12 +148,11 @@ end
 Base.@kwdef struct Tracer
   key::String
   callback::Function
-  rate::Float64
+  rate::Float64 = 1.0
 end
 
-function Tracer(key::String, callback::Function)
-  Tracer(key, callback, 1.0)
-end
+
+
 
 """
 Returns a vector of indices, sorted according to fitness.
