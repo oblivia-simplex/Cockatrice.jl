f927eea691d2b0128c24d731a9ce6e074a62e035
Lucca Fraser <lucca.fraser@gmail.com>
2021-04-29 12:40:38 -0300

    typefix

--- src/Geo.jl
+++ src/Geo.jl
@@ -147,7 +147,7 @@ end
 """
 Returns a vector of indices, sorted according to fitness.
 """
-function tournament(geo::Geography, fitness_function::Function; trace::Vector{NamedTuple(:key, :callback)}=[])
+function tournament(geo::Geography, fitness_function::Function; trace::Vector{NamedTuple{(:key, :callback)}}=[])
   indices = choose_combatants(geo, geo.config.population.t_size)
   #=Threads.@threads=# for i in indices
     geo.deme[i].fitness = fitness_function(geo.deme[i])
