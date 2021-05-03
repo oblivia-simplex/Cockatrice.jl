f7c11bd5f3a403dae882689905d609298e4a6c4f
Lucca Fraser <lucca.fraser@gmail.com>
2021-05-03 17:14:03 -0300

    refactoring

--- src/Evolve.jl
+++ src/Evolve.jl
@@ -90,7 +90,7 @@ end
 
 
 function preserve_elites!(evo::Evolution)
-  pop = sort([vec(evo.geo); evo.elites])
+  pop = sort([vec(evo.geo); evo.elites], by=(x -> x.fitness))
   n_elites = evo.config.population.n_elites
   evo.elites = [deepcopy(pop[end-i]) for i in 0:(n_elites-1)]
 end
