2399c565470d9959e7d1d2728be1607abd85c971
Lucca Fraser <lucca.fraser@gmail.com>
2021-05-04 22:22:02 -0300

    removed dead mod

--- src/Cockatrice.jl
+++ src/Cockatrice.jl
@@ -7,7 +7,6 @@ include("Config.jl")
 include("Geo.jl")
 include("Evo.jl")
 include("Cosmos.jl")
-include("examples/LinearGP.jl")
 
 if "COCKATRICE_VIS" ∈ keys(ENV) && ENV["COCKATRICE_VIS"] == 1
     include("Vis.jl")
